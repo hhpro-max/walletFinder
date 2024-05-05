7f66d57fb71570576611f6e278102bd78bd62684
hossein <seyed.h.hashemi1@gmail.com>
2024-05-05 11:48:20 -0700

    returned to the not threaded application

--- main.py
+++ main.py
@@ -4,20 +4,31 @@ from checkAddress import checkAddress
 checkAdd = checkAddress()
 addGen = AddressGen()
 api_url= "https://bitcoinexplorer.org/api/address/"
-
-def checkPPKH():
-    while True:
-        private_key,public_key,address = addGen.btcPPKH()
-        url = api_url + address
-        checkAdd.checkAddress(private_key=private_key,public_key=public_key,url=url,fileName="ppkh_address.txt")
-def checkPSH():
-    while True:
-        private_key,public_key,address = addGen.btcPSH()
-        url = api_url + address
-        checkAdd.checkAddress(private_key=private_key,public_key=public_key,url=url,fileName="psh_address.txt")      
 #here we wanted to use threads instead but we would get 503 error too much which i think its bicause of requests payload
+
+#def checkPPKH():
+#    while True:
+#        private_key,public_key,address = addGen.btcPPKH()
+#        url = api_url + address
+#        checkAdd.checkAddress(private_key=private_key,public_key=public_key,url=url,fileName="ppkh_address.txt")
+#def checkPSH():
+#    while True:
+#        private_key,public_key,address = addGen.btcPSH()
+#        url = api_url + address
+#        checkAdd.checkAddress(private_key=private_key,public_key=public_key,url=url,fileName="psh_address.txt")      
 #ppkh_thread = threading.Thread(target=checkPPKH)
 #ppkh_thread.start()
 #
 #psh_thread = threading.Thread(target=checkPSH)
 #psh_thread.start()
+def checkPPKH(): 
+    private_key,public_key,address = addGen.btcPPKH()
+    url = api_url + address
+    checkAdd.checkAddress(private_key=private_key,public_key=public_key,url=url,fileName="ppkh_address.txt")
+def checkPSH():
+    private_key,public_key,address = addGen.btcPSH()
+    url = api_url + address
+    checkAdd.checkAddress(private_key=private_key,public_key=public_key,url=url,fileName="psh_address.txt")  
+while True :
+    checkPPKH()
+    checkPSH()
